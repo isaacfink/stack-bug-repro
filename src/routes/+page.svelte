461b82d2ff34aead8a7927db6b33069d855758ea
isaac finkelstein <isaacfink123@gmail.com>
2023-10-13 01:10:20 +0100

    added description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,3 +20,7 @@
 {#each $stack.current as item }
     <Item bind:item/>
 {/each}
+
+<p class="">
+    Open console and click the add button, for some reason there is a second update happening after then first one and the value passed to that updfate is the value of the entire store not just $stack.current
+</p>
